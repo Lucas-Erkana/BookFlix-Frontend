7acb3ae9c18fe8064d16a084f3cb275553d66679
torobucci <kevintoro254@gmail.com>
2023-07-26 00:05:22 +0300

    configure store and add moviesSlice

---
+++ src/redux/MoviesSlice.js
@@ -0,0 +1,46 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  movies: [],
+};
+
+const moviesSlice = createSlice(
+  {
+    name: 'movies',
+    initialState,
+    reducers: {
+      setMovies(state, action) {
+        state.movies = action.payload;
+      },
+    },
+  },
+);
+
+export const { setMovies } = moviesSlice.actions;
+
+const fetchMovies = () => async (dispatch) => {
+  const response = await fetch('http://localhost:3000/api/v1/services');
+  const data = await response.json();
+  dispatch(setMovies(data));
+};
+
+const addMovie = (movie) => async (dispatch) => {
+  await fetch('http://localhost:3000/api/v1/services', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(movie),
+  });
+  dispatch(fetchMovies());
+};
+
+const deleteMovie = (id) => async (dispatch) => {
+  await fetch(`http://localhost:3000/api/v1/services/${id}`, {
+    method: 'DELETE',
+  });
+  dispatch(fetchMovies());
+};
+
+export { fetchMovies, addMovie, deleteMovie };
+export default moviesSlice.reducer;
