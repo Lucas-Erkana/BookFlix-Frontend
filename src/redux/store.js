146f4890ad5a091fc606b451f94e821cf605f2d5
Lucas David Erkana <lderkana21x@gmail.com>
2023-07-27 08:02:28 +0200

    imported from the correct file path
    -Renamed Userslice.js to UsersSlice.js
    -Remove the eslint disable

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
 import MoviesSlice from './MoviesSlice';
-// eslint-disable-next-line import/extensions
 import userReducer from './UsersSlice';
 
 const store = configureStore({
