6324edbc1ece1f28d1e50b8de8ff5396684d845b
SabaAhmad404 <sabaahmad895@gmail.com>
2023-07-27 20:39:51 +0500

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import LayOut from './components/view/LayOut';
-import Movie from './components/routes/Movie';
+import Home from './components/view/Home';
 import AddMovie from './components/routes/AddMovie';
 import SignIn from './components/view/SignIn';
+import SignUp from './components/view/SignUp';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles/custom.scss';
 
@@ -12,9 +13,10 @@ function App() {
     <div className="container-fluid col-md-12 d-flex p-0">
       <Routes>
         <Route path="/" element={<LayOut />}>
-          <Route index element={<Movie />} />
+          <Route index element={<Home />} />
           <Route path="/add-movie" element={<AddMovie />} />
           <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
         </Route>
       </Routes>
     </div>
