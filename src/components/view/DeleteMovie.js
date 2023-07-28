c72614230f9fb755c3512f24b14025830022d1f7
SabaAhmad404 <sabaahmad895@gmail.com>
2023-07-28 19:48:18 +0500

    Create delete movie functionality

---
+++ src/components/view/DeleteMovie.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchMovies } from '../../redux/MoviesSlice';
+import DeleteMovieCard from './DeleteMovieCard';
+
+const DeleteMovie = () => {
+  const dispatch = useDispatch();
+  const movies = useSelector((state) => state.movies.movies);
+
+  useEffect(() => {
+    dispatch(fetchMovies());
+  }, [dispatch]);
+  return (
+    <div className="container-fluid p-5">
+      {movies.map((movie) => (
+        <div className="row g-3 p-3" key={movie.id}>
+
+          <DeleteMovieCard
+            key={movie.id}
+            id={movie.id}
+            name={movie.name}
+            image={movie.image}
+            details={movie.details}
+          />
+
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default DeleteMovie;
