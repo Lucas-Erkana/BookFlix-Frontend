fe3a541969dbc267ad914e5f01a28283835d2634
torobucci <kevintoro254@gmail.com>
2023-07-28 16:51:45 +0300

    update the api url in locationSlice and reservationSlice

--- src/redux/LocationsSlice.js
+++ src/redux/LocationsSlice.js
@@ -19,7 +19,7 @@ export const { setLocations } = locationSlice.actions;
 export default locationSlice.reducer;
 
 const fetchLocations = () => async (dispatch) => {
-  const response = await fetch('http://localhost:3000/api/v1/locations');
+  const response = await fetch('https://book-flix.onrender.com/api/v1/locations');
   const data = await response.json();
   dispatch(setLocations(data));
 };
