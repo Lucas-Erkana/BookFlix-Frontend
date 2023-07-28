16b06e3071682699e8051f3e19beb27d5e6adf55
SabaAhmad404 <sabaahmad895@gmail.com>
2023-07-28 13:42:30 +0500

    updated App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './components/view/Home';
 import AddMovie from './components/routes/AddMovie';
 import SignIn from './components/view/SignIn';
 import SignUp from './components/view/SignUp';
+import Details from './components/view/Details';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles/custom.scss';
 
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LayOut />}>
           <Route index element={<Home />} />
+          <Route exact path="/services/:id" element={<Details />} />
           <Route path="/add-movie" element={<AddMovie />} />
           <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
