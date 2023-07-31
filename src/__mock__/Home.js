2f38e74285492915596330b77030ab415c2f2cb2
Lucas David Erkana <lderkana21x@gmail.com>
2023-07-31 12:14:00 +0200

    Test Home and Signin

---
+++ src/__mock__/Home.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+const Home = () => {
+  const cards = [{
+    name: 'Service 1',
+    image: 'https://res.cloudinary.com/dx0nauane/image/upload/v1624349663/Service%20Card%20Images/Service_1_1_xqjz3c.jpg',
+    details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod, sapien vitae aliquam dapibus, magna quam ultricies nunc, nec tincidunt nisl nunc vitae elit. ',
+    price: 100,
+    id: 1,
+  }];
+
+  const isMobile = window.innerWidth <= 768;
+
+  return (
+    <BrowserRouter>
+      <div className="home col-md col">
+        <h1 className="bold-font homepage-heading">
+          MOVIES TO WATCH
+        </h1>
+        <p className="gray-font">
+          Please select a Movie to reserve
+        </p>
+        <hr className="dash home-dash" />
+        <div className="carousel-container col-12">
+
+          <div>
+            {cards.map((card) => (
+              <div
+                key={card.id}
+              >
+                <div>
+                  {card.name}
+                  {card.image}
+                  {card.details}
+                  {card.price}
+                  {card.id}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default Home;
